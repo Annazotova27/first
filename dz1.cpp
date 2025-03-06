d45f19951a0bf2be00f4004b2b96a0ce9c7ccb6f
Annazotova27 <annazotova09@yandex.ru>
2025-03-06 20:51:03 +0300

    dz1 final

--- dz1.cpp
+++ dz1.cpp
@@ -19,23 +19,7 @@ void ShowEven(const int* array, size_t size_ar){
   }
 }
 
-int main(){
-  const int size=3;   
-  int ar[size];
-      for (int i=0;i<size;i++ )
-      {
-          cout<<"Введите число №"<<i+1<<"-> ";
-          cin>>ar[i];
-          cout<<endl;
-      }
-  cout<<endl;
-  ShowNum_reverse(ar, size);
-  cout<<endl;
-  ShowNum(ar, size);
-  cout<<endl;
-  ShowEven(ar, size);
-  cout<<endl;
-}
+
 
  
 

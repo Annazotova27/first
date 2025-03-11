27b9320560414aa68510c72093456712bf8160fc
Annazotova27 <annazotova09@yandex.ru>
2025-03-11 13:48:43 +0300

    dz1 latest plz

---
+++ dz1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "dz1.h"
+using namespace std;
+void ShowNum_reverse(const int* array, size_t size_ar){
+  for (int i = size_ar; i >=0; i--){
+    cout<<array[i]<<'\t';
+  }
+}
+  void ShowNum(const int* array, size_t size_ar){
+    for (int i=0; i<size_ar; i++){
+      cout<<array[i]<<'\t';
+    }
+  }
+void ShowEven(const int* array, size_t size_ar){
+  for (int i=0; i<size_ar;i++){
+    if (array[i]==array[i]/2*2){
+      cout<<array[i]<<'\t';
+    }
+  }
+}
+
+
+
+ 
+
--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
-int main(int argc, char** argv){
-	for (int i = 0; i < argc; i++){
-		std::cout << i << ":" << argv[i] << std::endl;
-
-	}
-	return 0;
+#include "dz1.h"
+using namespace std;
+int main(){
+  const int size=3;   
+  int ar[size];
+      for (int i = 0;i<size;i++ )
+      {
+          cout<<"Введите число №"<<i+1<<"-> ";
+          cin>>ar[i];
+          cout<<endl;
+      }
+  cout<<endl;
+  ShowNum_reverse(ar,size);
+  cout<<endl;
+  ShowNum(ar, size);
+  cout<<endl;
+  ShowEven(ar, size);
+  cout<<endl;
 }
+
  
 
 

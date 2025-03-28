6180168f1293839508e3502cfa62150450be22d6
Annazotova27 <annazotova09@yandex.ru>
2025-03-28 18:08:05 +0300

    pls

--- main.cpp
+++ main.cpp
@@ -1,24 +1,11 @@
-#include <iostream>
-#include "array_info.h"
-int main(int,char**){
-      int *size=new int;
-  int *number=new int;
-  int *max=new int;
-  int *min=new int;
-  int *ar=new int [*size];
-  ar= CreateAr(*size);
-  delete[] ar;
-  ar= CreateAr(*size,*number);
-  delete[] ar;
-  ar=CreateAr(*size,*max,*min);
-  delete[] ar;
-  delete size;
-  delete number;
-  delete max;
-  delete min;
-  
-  
-  }
+
+ #include <iostream>
+int main(int argc, char** argv) {
+for (int i = 0; i < argc; i++) {
+std::cout << i << “ : “ << argv[i]  << std::endl;
+}
+return 0;
+}
 
 
 

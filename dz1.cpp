848e67039e174195de5432a73160c5b4c6f73b31
Annazotova27 <annazotova09@yandex.ru>
2025-03-28 20:37:33 +0300

    rain

--- dz1.cpp
+++ dz1.cpp
@@ -1,22 +1,32 @@
-#include <iostream>
 #include "dz1.h"
-using namespace std;
-void ShowNum_reverse(const int* array, size_t size_ar){
-  for (int i = size_ar; i >=0; i--){
-    cout<<array[i]<<'\t';
-  }
+#include <iostream>
+
+void ShowNum_reverse (const int* array, std::size_t size_ar){
+	for (int i = size_ar-1; i > 0; i--){
+		std::cout << array [i] << '\t';
+	}
+	
+	std::cout << array [0] << '\n';
+
 }
-  void ShowNum(const int* array, size_t size_ar){
-    for (int i=0; i<size_ar; i++){
-      cout<<array[i]<<'\t';
-    }
-  }
-void ShowEven(const int* array, size_t size_ar){
-  for (int i=0; i<size_ar ; i++){
-    if (array[i]==array[i]/2*2){
-      cout<<array[i]<<'\t';
-    }
-  }
+
+void ShowNum (const int* array, std::size_t size_ar){
+	for (int i = 0; i < size_ar-1; i++){
+		std::cout << array [i] << '\t';
+	}
+			
+	std::cout << array [size_ar-1] << '\n';
+}
+
+void ShowEven (const int* array, std::size_t size_ar){
+	for (int i = 0; i < size_ar-1; i++){
+		if ((array [i] & (0x01)) == 0){
+			std::cout << array[i] << '\t';
+		}
+	}			
+	if ((array [size_ar-1] & (0x01)) == 0){
+		std::cout << array[size_ar-1];
+	}	
 }
 
 

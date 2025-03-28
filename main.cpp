46fc9d3e938a04ce0517f65d2b72aca123b1d5a4
Annazotova27 <annazotova09@yandex.ru>
2025-03-28 20:45:23 +0300

    rain

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
+#include "dz1.h"
+#include <iostream>
 
- #include <iostream>
-int main(int argc, char** argv) {
-for (int i = 0; i < argc; i++) {
-std::cout << i << “ : “ << argv[i]  << std::endl;
+
+int main(int, char**){
+	int a[10];
+	ShowNum_reverse(a, (sizeof(a)/sizeof(a[0])));
+	ShowNum(a,(sizeof(a)/sizeof(a[0])));
+	ShowEven(a,(sizeof(a)/sizeof(a[0])));
 }
-return 0;
-}
+
 
 
 

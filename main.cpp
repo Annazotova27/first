b71f9908072f08f9e78d225f5a3d14514241b542
Annazotova27 <annazotova09@yandex.ru>
2025-04-05 14:16:21 +0300

    new

--- main.cpp
+++ main.cpp
@@ -1,17 +1,62 @@
-
 #include <iostream>
-#include  "sorts.h"
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+#include "statarray.h"
 
-int main(){
-  
-  int* ar; 
-  int size; 
-  double* probabilities; 
-  int size_prob;
-  struct StatArray GetStatArray(const int* ar, int size);
-  void insertion_sort(int* ar, int size, bool isAscending);
-  void bubble_sort(int* ar, int size, bool isAscending);
+
+void printIntArray(const int* arr, int size) {
+    std::cout << "[";
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i];
+        if (i < size - 1) std::cout << ", ";
+    }
+    std::cout << "]" << std::endl;
 }
+
+
+void printProbabilityArray(const double* arr, int size) {
+    std::cout << "Значение -> Вероятность:" << std::endl;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > 0) {
+            std::cout << "  " << i << " -> " << std::fixed << std::setprecision(4) << arr[i] << std::endl;
+        }
+    }
+}
+
+void freeStatArray(StatArray& stat) {
+    delete[] stat.ar;
+    delete[] stat.probabilities;
+}
+
+int main() {
+    srand(static_cast<unsigned int>(time(nullptr)));
+    
+    
+    int size1 = 10;
+    int* array1 = new int[size1]{5, 5, 5, 10, 10, 20, 20, 20, 20, 30};
+    
+    
+    std::cout << "Массив: ";
+    printIntArray(array1, size1);
+    
+    
+    StatArray stat1 = GetStatArray(array1, size1);
+    
+    
+    std::cout << "\nИнформация о StatArray:" << std::endl;
+    std::cout << "Размер исходного массива: " << stat1.size << std::endl;
+    std::cout << "Размер массива вероятностей: " << stat1.size_prob << std::endl;
+    std::cout << "Вероятности элементов:" << std::endl;
+    printProbabilityArray(stat1.probabilities, stat1.size_prob);
+    
+    
+    delete[] array1;
+    freeStatArray(stat1);
+    
+    return 0;
+}
+
 
 
 

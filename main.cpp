64a44afa38f6bb8f84191fd0c3dde2a33123f99b
Annazotova27 <annazotova09@yandex.ru>
2025-04-10 21:13:11 +0300

    ne

--- main.cpp
+++ main.cpp
@@ -1,26 +1,9 @@
 #include <iostream>
 #include "array_info.h"
-int main(int,char**){
-      int *size=new int;
-  int *number=new int;
-  int *max=new int;
-  int *min=new int;
-  int *ar=new int [*size];
-  ar= CreateAr(*size);
-  delete[] ar;
-  ar= CreateAr(*size,*number);
-  delete[] ar;
-  ar=CreateAr(*size,*max,*min);
-  delete[] ar;
-  delete size;
-  delete number;
-  delete max;
-  delete min;
  #include <iostream>
 #include <iomanip>
 #include <cstdlib>
 #include <ctime>
-#include "statarray.h"
 
 void printIntArray(const int* arr, int size) {
     std::cout << "[";
